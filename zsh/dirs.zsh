451b59647285d8a1e183546bc571401128006025
Dinesh Majrekar <dinesh.majrekar@serverchoice.com>
2012-05-09 14:56:36 +0100

    Set ~portal path for macs

--- zsh/dirs.zsh
+++ zsh/dirs.zsh
@@ -1,2 +1,6 @@
-portal=/home/dmajrekar/src/portal
+if [ -d /home/dmajrekar/src/portal ]; then
+ portal=/home/dmajrekar/src/portal
+elif [ -d /Users/dmajrekar/src/portal ]; then
+  portal=/Users/dmajrekar/src/portal
+fi
 : ~portal
