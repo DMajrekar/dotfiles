e4eb5e1bc7e0857d85fd546ef71dc9241b8ecdc2
Dinesh Majrekar <dinesh.majrekar@serverchoice.com>
2013-02-19 14:14:32 +0000

    Updated zsh dirs

--- zsh/dirs.zsh
+++ zsh/dirs.zsh
@@ -1,6 +1,7 @@
 if [ -d /home/dmajrekar/src/portal ]; then
- portal=/home/dmajrekar/src/portal
+  portal=/home/dmajrekar/src/portal
+  : ~portal
 elif [ -d /Users/dmajrekar/src/portal ]; then
   portal=/Users/dmajrekar/src/portal
+  : ~portal
 fi
-: ~portal
